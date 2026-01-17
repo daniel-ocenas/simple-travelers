87de39fd8e10cab2cd4967473d147dbe728bbded
DanielOcenas <danielm.ocenas@gmail.com>
2026-01-17 22:51:21 +0100

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,12 @@
 import withPlaiceholder from '@plaiceholder/next';
 
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactCompiler: true,
   turbopack: {},
   output: 'export',
+  basePath: '/simple-travelers',
+  assetPrefix: '/simple-travelers/',
   images: {
     unoptimized: true,
   },
